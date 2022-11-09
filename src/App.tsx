4920758463ba3d7798f59bba664192538e5d2de0
WeslleySOR <weslleywruas@gmail.com>
2022-11-09 13:32:04 -0300

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,25 @@ function App() {
     ILotteryAndOurContest[]
   >([]);
 
+  const actualLotteryName = () => {
+    switch (actualLotteryPage) {
+      case 0:
+        return "bg-mega-sena";
+      case 1:
+        return "bg-quina";
+      case 2:
+        return "bg-loto-facil";
+      case 3:
+        return "bg-loto-mania";
+      case 4:
+        return "bg-time-mania";
+      case 5:
+        return "bg-dia-de-sorte";
+      default:
+        return "";
+    }
+  };
+
   const handleLotterySelectChange = (
     e: React.ChangeEvent<HTMLSelectElement>
   ) => {
@@ -100,19 +119,60 @@ function App() {
   }, []);
 
   return (
-    <main className="w-full h-screen border-4 border-solid border-emerald-900">
+    <>
       {lotteryList.length > 0 ? (
-        <select
-          value={actualLotteryPage}
-          onChange={(e) => handleLotterySelectChange(e)}
-        >
-          {lotteryList.map((lottery) => (
-            <option value={lottery.id}>{lottery.nome.toUpperCase()}</option>
-          ))}
-        </select>
+        <main className="w-full h-screen flex flex-col max-w-screen overflow-x-hidden lg:flex-row">
+          <div
+            className={`${actualLotteryName()} relative flex flex-col gap-20 items-center py-16 lg:p-24
+           after:absolute after:content-[''] 
+           after:w-[110vw] after:h-24 after:bg-[#efefef] after:-bottom-12  after:rounded-t-[100%]
+           lg:after:bottom-0 lg:after:h-[100vh] lg:after:w-14 lg:after:-right-8 lg:after:rounded-[100%]
+          `}
+          >
+            <select
+              className="bg-[#fff] py-4 px-6 rounded-lg drop-shadow-sm w-64"
+              value={actualLotteryPage}
+              onChange={(e) => handleLotterySelectChange(e)}
+            >
+              {lotteryList.map((lottery) => (
+                <option value={lottery.id}>{lottery.nome.toUpperCase()}</option>
+              ))}
+            </select>
+            <div className="flex flex-col items-center gap-3">
+              <div className="relative w-[65px] h-16">
+                <img className="absolute" src="/assets/Path 2.svg" alt="" />
+                <img className="absolute" src="/assets/Path 3.svg" alt="" />
+              </div>
+              <h1 className="text-white text-3xl font-bold uppercase">
+                {
+                  lotteryList.filter(
+                    (lottery) => lottery.id === actualLotteryPage
+                  )[0].nome
+                }
+              </h1>
+            </div>
+            <h2 className="text-white text-base font-medium uppercase">
+              Concurso Nº {actualContestPage?.id}
+            </h2>
+          </div>
+          {actualContestPage ? (
+            <div className="flex flex-col pb-8 lg:w-full">
+              <div className="flex flex-wrap mt-auto items-center justify-center px-12 z-50 gap-6">
+                {actualContestPage.numeros.map((numero) => (
+                  <div className="w-[76px] h-[76px] flex items-center justify-center rounded-full bg-white">
+                    <p className="text-xl font-bold text-[#333333]">{numero}</p>
+                  </div>
+                ))}
+              </div>
+              <span className="text-black text-sm font-normal text-center mt-20 lg:mt-auto">
+                Este sorteio é meramente ilustrativo e não possui nenhuma
+                ligação com a CAIXA.
+              </span>
+            </div>
+          ) : null}
+        </main>
       ) : null}
-      {actualContestPage ? actualContestPage.numeros.map(numero => <p>{numero}</p>) : null}
-    </main>
+    </>
   );
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    fontFamily: {
+      montSerrat: ["Montserrat, sans-serif"],
+    },
+    extend: {
+      colors: {
+        "mega-sena": "#6BEFA3",
+        quina: "#8666EF",
+        "loto-facil": "#DD7AC6",
+        "loto-mania": "#FFAB64",
+        "time-mania": "#5AAD7D",
+        "dia-de-sorte": "#BFAF83",
+      },
+    },
   },
   plugins: [],
 };
